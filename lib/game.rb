b917a5073678df575ce14f3c619f45b9ab938d51
Jose Quinones <josemanuelqn.88@gmail.com>
2019-04-20 09:04:17 +0100

    Add method ships to Game to pretty-print the ships names, qty and length

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,16 @@
 class Game
+  SHIPS = {
+    carrier: [1, 5],
+    battleship: [2, 4],
+    cruiser: [3, 3],
+    submarine: [4, 3],
+    destroyer: [5, 2]
+  }
+  def ships
+    string = ''
+    SHIPS.each do |ship, details|
+      string += "#{ship.capitalize} Qty: #{details.first} Length: #{details.last}\n"
+    end
+    puts string
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,19 @@
 require 'game'
 
 describe Game do
+  subject(:game) { Game.new }
+
+  describe '#ships' do
+    it 'pretty-print the ships, qty and their length' do
+      ships = 'Carrier Qty: 1 Length: 5
+Battleship Qty: 2 Length: 4
+Cruiser Qty: 3 Length: 3
+Submarine Qty: 4 Length: 3
+Destroyer Qty: 5 Length: 2
+'
+
+      expect(STDOUT).to receive(:puts).with ships
+      game.ships
+    end
+  end
 end
