b5be656b9ddc1946aa21767aea7f81dfc7f7b1a6
Jose Quinones <josemanuelqn.88@gmail.com>
2019-05-16 16:24:04 +0100

    Battle_Ship server initializes a Game object

--- lib/battle_ship.rb
+++ lib/battle_ship.rb
@@ -1,10 +1,15 @@
 require 'socket'
+require_relative 'game'
+require_relative 'grid'
 
 class Battle_Ship
+
+  attr_accessor :game
   
-  def initialize
+  def initialize(game = Game.new(grid = Grid.new))
     @port = 2979
     @server = TCPServer.open(@port)
+    @game = game
   end
 
   def accept_client
--- spec/battle_ship_spec.rb
+++ spec/battle_ship_spec.rb
@@ -2,12 +2,16 @@ require 'battle_ship'
 require 'socket'
 
 describe Battle_Ship do
+  
+  let(:game) {double (:game)}
+
   before(:each) do
-    @server = Battle_Ship.new
-    Thread.new{@server.accept_client}
+    @server = Battle_Ship.new(game)
+    @accept = Thread.new{@server.accept_client}
   end
 
   after(:each) do
+    @accept.join
     @server.close
   end
 
@@ -15,4 +19,17 @@ describe Battle_Ship do
     client = TCPSocket.new('localhost', 2979)
     expect(client.gets.chomp).to eq('Welcome to BattleShip!!!')
   end
+
+  it 'initializes a game object' do
+    client = TCPSocket.new('localhost', 2979)
+    expect(@server.game).to eq(game)
+  end
+
+  it 'assign clients to player 1 and player 2'
+
+  it 'allow players to position their ships'
+
+  it 'loops through shooting rounds'
+
+  it 'ends when all ships from a player are destroyed'
 end
