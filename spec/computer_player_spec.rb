29a4754bbb399420e5031458378aaa69ec4025c6
Jose Quinones <josemanuelqn.88@gmail.com>
2019-05-28 13:59:04 +0100

    Add computer_player

---
+++ spec/computer_player_spec.rb
@@ -0,0 +1,11 @@
+require 'computer_player'
+
+describe Computer_Player do
+  describe '#shots' do
+    it 'declares shots'
+  end
+
+  decribe '#position_ships' do
+    it 'position ship'
+  end
+end
