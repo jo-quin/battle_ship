b57396203bd2bfea24edbb51087e1a08f0e2fad1
Jose Quinones <josemanuelqn.88@gmail.com>
2019-06-25 13:33:36 +0100

    Update average_rounds_test to use the current working directory

--- spec/average_rounds_test.rb
+++ spec/average_rounds_test.rb
@@ -1,10 +1,10 @@
 require 'date'
 
 10.times do
-  t1 = Thread.new {system('ruby /Users/student/scripts/battle_ship/lib/battle_ship.rb')}
+  t1 = Thread.new {system("ruby #{Dir.pwd}/lib/battle_ship.rb test")}
   sleep 0.5
-  t2 = Thread.new {system('ruby /Users/student/scripts/battle_ship/spec/functional_test.rb')}
-  t3 = Thread.new {system('ruby /Users/student/scripts/battle_ship/spec/functional_test.rb')}
+  t2 = Thread.new {system("ruby #{Dir.pwd}/lib/computer_player.rb")}
+  t3 = Thread.new {system("ruby #{Dir.pwd}/spec/functional_test.rb")}
 
   t1.join
   t2.join
@@ -13,22 +13,22 @@ end
 
 rounds = 0
 
-open('/Users/student/scripts/battle_ship/spec/average_rounds.txt', 'r') do |file| 
+open("#{Dir.pwd}/spec/average_rounds.txt", 'r') do |file| 
   file.each_line do |line|
     rounds += line.split(' ')[1].to_i
   end
 end
 
-open('/Users/student/scripts/battle_ship/spec/average_rounds.txt', 'w') do |f| 
+open("#{Dir.pwd}/spec/average_rounds.txt", 'w') do |f| 
   f.puts
 end
 
 average_rounds = rounds/10.to_f
 
-open('/Users/student/scripts/battle_ship/spec/logs/average_rounds_log.txt', 'a') do |f| 
+open("#{Dir.pwd}/spec/logs/average_rounds_log.txt", 'a') do |f| 
     f.puts
     f.puts "\u001b[101m#{DateTime.now.strftime}: AVERAGE ROUNDS: #{average_rounds}\u001b[0m"
     f.puts
   end
 
-system('cat /Users/student/scripts/battle_ship/spec/logs/average_rounds_log.txt')
+system("cat #{Dir.pwd}/spec/logs/average_rounds_log.txt")
